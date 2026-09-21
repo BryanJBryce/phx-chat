cfe4a2cf7ed89b8b5ecf29d52b97b7187b05e571
BryanJBryce <bryan@brycelabs.com>
2026-09-21 09:12:17 -0700

    Add Credo and enable ex_slop checks
    Install both tools as development and test dependencies and register the ExSlop plugin in the Credo configuration.

--- mix.exs
+++ mix.exs
@@ -48,6 +48,8 @@ defmodule App.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.2.0"},
       {:lazy_html, ">= 0.1.0", only: :test},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_slop, "~> 0.4.4", only: [:dev, :test], runtime: false},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.5", runtime: Mix.env() == :dev},
